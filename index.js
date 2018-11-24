350f93032e2cdfe2d0ac870a8bee3d47a556f30e
Evan Goodwin <escottgoodwin@gmail.com>
2018-11-23 16:57:19 -0800

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var cron = require('node-cron');
 
+// ┌────────────── second (optional)
+// │ ┌──────────── minute
+// │ │ ┌────────── hour
+// │ │ │ ┌──────── day of month
+// │ │ │ │ ┌────── month
+// │ │ │ │ │ ┌──── day of week
+// │ │ │ │ │ │
+// │ │ │ │ │ │
+// * * * * * *
+
 cron.schedule('* * * * *', () => {
   console.log('running a task every minute');
 });
